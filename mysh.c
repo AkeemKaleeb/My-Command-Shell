dca0a0645b0f09291d44a0b26e51d8641f36096b
AkeemKaleeb <kaileb.j.cole@gmail.com>
2024-03-21 15:10:49 -0400

    Echo command
    Implementation of an echo command

--- mysh.c
+++ mysh.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_INPUT 1024      // Current Max input size, can be reallocated
 
@@ -13,7 +15,9 @@ void myshLoop() {
     printf("Shell started: enjoy!\n");
 
     while (true) {              // Shell loop
-        printf("> ");           // Every new line receives shell "> "
+
+/********************************************Read Line********************************************/
+        printf("mysh> ");       // Every new line receives shell "> "
         fflush(stdout);         // Text is output to the shell console
 
         bytes_read = read(STDIN_FILENO, input, MAX_INPUT);      // read() buffer
@@ -25,6 +29,23 @@ void myshLoop() {
 
         if (strcmp(input, "exit") == 0) {       // If the user inputs "exit" end the shell program loop, exiting the program
             break;
+        }
+
+/****************************************Tokenize Commands****************************************/
+        char* token = strtok(input, " ");           // Tokenize the input
+        if (token != NULL) {                        // Check the first token (command)
+
+            if (strcmp(token, "echo") == 0) {       // echo Command
+                token = strtok(NULL, " ");          // tokenize the remainder of the word
+                while (token != NULL) {             // Print the rest of the tokens
+                    printf("%s ", token);
+                    token = strtok(NULL, " ");
+                }
+                printf("\n");
+            } 
+            else {
+                printf("Unknown command: %s\n", token);
+            }
         }
     }
 }
