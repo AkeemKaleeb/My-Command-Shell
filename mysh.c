a44cb4374dcd53f6fbcdfe54e287f39d04b7c16f
AkeemKaleeb <kaileb.j.cole@gmail.com>
2024-03-28 11:06:48 -0400

    Extracted Methods
    Extracted the methods for reading a line and tokenizing the arguments into separate functions and fixed memory related issues along with them

--- mysh.c
+++ mysh.c
@@ -3,51 +3,84 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
-#define MAX_INPUT 1024      // Current Max input size, can be reallocated
+#define MAX_INPUT 1024                  // Current Max input size, can be reallocated
+#define TOKEN_DELIMITER " \t\r\n\a"     // Characters that divide command line arguments
+
+void exitShell(int exitStatus) {
+    exit(exitStatus);
+}
+
+char* readLine() {
+    char* input = malloc(MAX_INPUT * sizeof(char*));
+    if(!input) {
+        fprintf(stderr, "Error allocating read line space");
+        exit(EXIT_FAILURE);
+    }
+
+    int bytes_read = read(STDIN_FILENO, input, MAX_INPUT);      // read() buffer
+    if (bytes_read < 0) {                                       // If there is a problem reading the input buffer, print error
+        fprintf(stderr, "Error reading the command line");
+        exitShell(EXIT_FAILURE);            
+    }
+    
+    input[bytes_read - 1] = '\0';           // Remove newline character at the end
+
+    if (strcmp(input, "exit") == 0) {       // If the user inputs "exit" end the shell program loop, exiting the program
+        exitShell(EXIT_SUCCESS);
+    }
+
+    return input;
+}
+
+char **tokenizeInput(char *input) {
+    char** tokens = malloc(MAX_INPUT * sizeof(char*));          // Allocate space for tokens array
+    if(!tokens) {
+        fprintf(stderr, "Error allocating space for tokens");
+        exitShell(EXIT_FAILURE);
+    }
+
+    char *token;
+    int position = 0;
+    int bufferSize = MAX_INPUT;
+
+    token = strtok(input, TOKEN_DELIMITER);
+    while(token != NULL) {
+        tokens[position] = token;           // Assign each token to the array
+        position++;                         // Increment the array
+
+        if(position >= bufferSize){         // Arguments too long, reallocate space
+            bufferSize += MAX_INPUT;
+            tokens = realloc(tokens, bufferSize * sizeof(char*));
+
+            if(!tokens) {              // Error reallocating space
+                fprintf(stderr, "Error reallocating token space");
+                exitShell(EXIT_FAILURE);
+            }
+        }           
+
+        token = strtok(NULL, TOKEN_DELIMITER);
+    }
+
+    tokens[position] = NULL;                // Last position increment holds no token
+    return tokens;
+}
 
 void myshLoop() {
-
-    char input[MAX_INPUT];      // Input text array
-    int bytes_read;
-
     printf("Shell started: enjoy!\n");
 
-    while (true) {              // Shell loop
-
-/********************************************Read Line********************************************/
+    do {                        // Shell loop
         printf("mysh> ");       // Every new line receives shell "> "
         fflush(stdout);         // Text is output to the shell console
 
-        bytes_read = read(STDIN_FILENO, input, MAX_INPUT);      // read() buffer
-        if (bytes_read < 0) {                                   // If there is a problem reading the input buffer, print error
-            perror("Error reading the command line");
-        }
-        
-        input[bytes_read - 1] = '\0';           // Remove newline character at the end
+        char *line = readLine();       // Assign global input to user input
+    
+        char **args = tokenizeInput(line);       
 
-        if (strcmp(input, "exit") == 0) {       // If the user inputs "exit" end the shell program loop, exiting the program
-            break;
-        }
-
-/****************************************Tokenize Commands****************************************/
-        char* token = strtok(input, " ");           // Tokenize the input
-        if (token != NULL) {                        // Check the first token (command)
-
-            if (strcmp(token, "echo") == 0) {       // echo Command
-                token = strtok(NULL, " ");          // tokenize the remainder of the word
-                while (token != NULL) {             // Print the rest of the tokens
-                    printf("%s ", token);
-                    token = strtok(NULL, " ");
-                }
-                printf("\n");
-            } 
-            else {
-                printf("Unknown command: %s\n", token);
-            }
-        }
-    }
+        free(line);
+        free(args);
+    } while(true);
 }
 
 int main() {
@@ -59,6 +92,7 @@ int main() {
 
     // cleanup and exiting
     printf("Exiting Shell. Have a nice day!\n");
+    exitShell(EXIT_SUCCESS);
 
     return 0;
 }
