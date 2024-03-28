56014f0e1bb1f403b80e529d3849b4affeb34f6a
AkeemKaleeb <kaileb.j.cole@gmail.com>
2024-03-28 12:28:07 -0400

    Comments and memory leak fix
    Added comments to understand what each part of the code does in its current state

--- mysh.c
+++ mysh.c
@@ -8,13 +8,15 @@
 #define MAX_INPUT 1024                  // Current Max input size, can be reallocated
 #define TOKEN_DELIMITER " \t\r\n\a"     // Characters that divide command line arguments
 
-void exitShell(int exitStatus) {
+// Built in function to exit the program with a given exit status
+int exitShell(int exitStatus) {
     exit(exitStatus);
 }
 
+// Function to read the user input to be used by the program
 char* readLine() {
-    char* input = malloc(MAX_INPUT * sizeof(char*));
-    if(!input) {
+    char* input = malloc(MAX_INPUT * sizeof(char*));            // Allocate space for input
+    if(!input) {                                                // If space wasn't allocated, report an error
         fprintf(stderr, "Error allocating read line space");
         exit(EXIT_FAILURE);
     }
@@ -34,18 +36,20 @@ char* readLine() {
     return input;
 }
 
+// Function to take an input line and tokenize each argument by white space
+// Returns an array of these tokenized arguments
 char **tokenizeInput(char *input) {
     char** tokens = malloc(MAX_INPUT * sizeof(char*));          // Allocate space for tokens array
-    if(!tokens) {
+    if(!tokens) {                                               // If space was not allocated, report an error
         fprintf(stderr, "Error allocating space for tokens");
         exitShell(EXIT_FAILURE);
     }
 
-    char *token;
-    int position = 0;
-    int bufferSize = MAX_INPUT;
+    char *token;                            // Individual token
+    int position = 0;                       // Incremental counter
+    int bufferSize = MAX_INPUT;             // Reallocatable buffer size
 
-    token = strtok(input, TOKEN_DELIMITER);
+    token = strtok(input, TOKEN_DELIMITER); // Tokenize the inputs
     while(token != NULL) {
         tokens[position] = token;           // Assign each token to the array
         position++;                         // Increment the array
@@ -60,29 +64,44 @@ char **tokenizeInput(char *input) {
             }
         }           
 
-        token = strtok(NULL, TOKEN_DELIMITER);
+        token = strtok(NULL, TOKEN_DELIMITER);      // Nullify the final position increment
     }
 
-    tokens[position] = NULL;                // Last position increment holds no token
+    tokens[position] = NULL;                        // Last position increment holds no token
     return tokens;
 }
 
+// Function to execute any built in commands
+// Returns true if the program should continue running
+int execute(char **args) {
+    if(args[0] == NULL) {           // Empty command, continue
+        return true;
+    }
+
+    return true;
+}
+
+// Shell Loop to handle running the program
 void myshLoop() {
     printf("Shell started: enjoy!\n");
+    char *line;
+    char **args;
+    int status;
 
     do {                        // Shell loop
         printf("mysh> ");       // Every new line receives shell "> "
         fflush(stdout);         // Text is output to the shell console
 
-        char *line = readLine();       // Assign global input to user input
-    
-        char **args = tokenizeInput(line);       
+        line = readLine();              // Read user input from terminal
+        args = tokenizeInput(line);     // Organize input line to an array of arguments
+        status = execute(args);
 
-        free(line);
+        free(line);                     // Free previous data
         free(args);
-    } while(true);
+    } while(status);
 }
 
+// Main method to initialize the program, begin running the loop, and close the program
 int main() {
     // preparation and initialization
     printf("Starting mysh, please wait...\n");
